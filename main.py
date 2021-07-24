ae09e9d786768a9b7350598d133e55303005d181
Rutujapingale10 <rutujapingale729@gmail.com>
2021-07-24 10:32:11 +0100

    waste giver and taker data added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
-import openpyxl as pxl
+"""import openpyxl as pxl
 import panda as pd
+
 
 login = pd.read_excel('login.xlsx')
 exp = pxl.load_workbook('login.xlsx')
 sheet = exp['Sheet']
-sheet['D1'] = 'hii'
+sheet['D1'] = 'hii'"""
 
 from streamlit.proto.Button_pb2 import Button 
 def main():
@@ -53,13 +54,29 @@ def main():
           password = st.sidebar.text_input("password",type='password')
           if st.sidebar.checkbox('Login'):
               st.success("Logged In as {}".format(username))
-              task = st.selectbox("Task",["Role","Waste Giver","Waste Taker"])
-              if task =="Task":
+              task = st.selectbox("Role",["Role","Waste Giver","Waste Taker"])
+              if task =="Role":
                   st.subheader("Add your Role")
               elif task == "Waste Giver":
-                  st.subheader("Waste Giver")
+                    st.subheader("Waste Giver")
+                   #user_name = st.write("Enter your name")
+                    Name_ = st.text_input("Enter your Name")
+                    email_id = st.text_input("Enter your email_id")
+                    contact_no = st.text_input("Enter your contact no")
+                    address = st.text_input("Enter your address")
+                    #type = st.text_input("Enter type of Waste")
+                    type= st.selectbox("Type",["type","Municipal solid waste","Hazardous waste","Biodegreadable waste","E-waste","Household hazardous waste"])
+
+
+
+
               elif task == "Waste Taker":
-                  st.subheader("Waste Taker")  
+                    st.subheader("Waste Taker")
+                    Name = st.text_input("Enter your Name")
+                    email_id = st.text_input("Enter your email_id")
+                    contact_no = st.text_input("Enter your contact no")
+                    address = st.text_input("Enter your address")
+                    type= st.selectbox("Type of waste you want for recycle",["type","Municipal solid waste","Hazardous waste","Biodegreadable waste","E-waste","Household hazardous waste"])  
 
 
 
